a3749c2e5b299754f2bbaa149cd9f6624cc07a1b
AdityaNarayanTiwari <104133590+Aditya3488@users.noreply.github.com>
2022-09-10 12:07:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Counting from './Components/Counting';
+
+function App() {
+  return (
+    <div className="App">
+      <Counting />
+    </div>
+  );
+}
+
+export default App;
